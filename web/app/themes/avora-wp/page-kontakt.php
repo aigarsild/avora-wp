9a3d39dddcd57587aa1f8dc279224fc96b409951
Aigar <aigarsild@gmail.com>
2025-09-04 20:16:39 +0300

    insta fix

--- web/app/themes/avora-wp/page-kontakt.php
+++ web/app/themes/avora-wp/page-kontakt.php
@@ -78,7 +78,7 @@ get_header(); ?>
                         <a href="https://www.facebook.com/profile.php?id=61566708334193" class="social-link" target="_blank" rel="noopener noreferrer">
                             <img src="<?php echo get_template_directory_uri(); ?>/images/icons/facebook-icon.svg" alt="Facebook" class="social-icon">
                         </a>
-                        <a href="https://www.instagram.com/mannisalu_kodud/" class="social-link" target="_blank" rel="noopener noreferrer">
+                        <a href="https://www.instagram.com/avora.ee/" class="social-link" target="_blank" rel="noopener noreferrer">
                             <img src="<?php echo get_template_directory_uri(); ?>/images/icons/instagram-icon.svg" alt="Instagram" class="social-icon">
                         </a>
                     </div>
